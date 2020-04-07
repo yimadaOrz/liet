6c32e80f129c0cf3932cea1208cd073c8b4cadb3
yimadaOrz <56173228+yimadaOrz@users.noreply.github.com>
2020-04-07 16:00:08 -0400

    Update BDM_HW3.py

--- BDM_HW3.py
+++ BDM_HW3.py
@@ -1,28 +1,33 @@
 from pyspark import SparkContext
-import csv
 import sys
+import csv
 
 
 if __name__=='__main__':
-      
+    
     sc = SparkContext()
-    
-    input_file = sys.argv[1]
+
+    input = sys.argv[1]
     output_folder = sys.argv[2]
-    
-    lines = sc.textFile(input_file).cache() 
-    header = lines.take(1)
-    lines = lines.filter(lambda row: row not in header) \
-            .mapPartitions(lambda line: csv.reader(line, delimiter=',', quotechar='"')) \
-            .filter(lambda row: len(row) > 7 and type(row[0]) == str) \
-            .filter(lambda row: len(row[0]) == 10) \
-            .map(lambda line: ((line[1], line[0][:4], line[7].lower()),1)) \
-            .reduceByKey(lambda x,y: x + y) \
-            .map(lambda x: ((x[0][0], x[0][1]), (x[0][2], x[1]))) \
-            .reduceByKey(lambda x,y: x + y) \
-            .map(lambda x: (x[0][0], x[0][1], sum(i for i in x[1][1::2]), 
-                                len(x[1][::2]),
-                                round(100*max(i for i in x[1][1::2])/sum(i for i in x[1][1::2]))
-                           )) \
-            .sortBy(lambda x: (x[0][0], x[0][1])) \
-            .saveAsTextFile(output_folder)
+
+    cpl = sc.textFile(input, use_unicode=True).cache()
+
+    def mapper(partitionId, records):
+        if partitionId == 0:
+            next(records)
+            reader = csv.reader(records,delimiter=',',quotechar='"')
+            for r in reader:
+                if len(r)>7 and type(r[0])==str and len(r[0])==10 and r[0][0]==('2'or'1'):
+                    yield r[0][0:4], \
+                        r[1].lower().replace('"', "'") if ',' in r[1] else r[1].lower(), \
+                        r[7].lower()
+                    
+
+    cpl.mapPartitionsWithIndex(mapper) \
+        .map(lambda x: ((x[1],x[0],x[2]),1)) \
+        .reduceByKey(lambda x,y: x+y) \
+        .map(lambda x: ((x[0][0],x[0][1]),(x[1],1,x[1]))) \
+        .reduceByKey(lambda x,y: (x[0]+y[0], x[1]+y[1], max(x[2],y[2]))) \
+        .mapValues(lambda x: (x[0],x[1], round(x[2]/x[0]*100))) \
+        .sortByKey() \
+        .saveAsTextFile(output_folder)
